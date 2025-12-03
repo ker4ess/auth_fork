4516a76380db8535d4df92a99a0acd628aac148d
ker4ess <76400544+ker4ess@users.noreply.github.com>
2025-12-03 17:45:34 +0300

    Add files via upload

---
+++ src/com/protect7/authanalyzer/filter/ToolSourceFilter.java
@@ -0,0 +1,120 @@
+package com.protect7.authanalyzer.filter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.protect7.authanalyzer.util.GenericHelper;
+import burp.IBurpExtenderCallbacks;
+import burp.IRequestInfo;
+import burp.IResponseInfo;
+
+public class ToolSourceFilter extends RequestFilter {
+	
+	private static final String TOOL_PROXY = "Proxy";
+	private static final String TOOL_REPEATER = "Repeater";
+	private static final String TOOL_INTRUDER = "Intruder";
+	private static final String TOOL_SCANNER = "Scanner";
+	private static final String TOOL_SPIDER = "Spider";
+	private static final String TOOL_SEQUENCER = "Sequencer";
+	private static final String TOOL_EXTENDER = "Extender";
+	private static final String TOOL_TARGET = "Target";
+	private static final String TOOL_COMPARER = "Comparer";
+	private static final String TOOL_COLLABORATOR = "Collaborator";
+	private static final String TOOL_LOGGER = "Logger";
+	
+	public ToolSourceFilter(int filterIndex, String description) {
+		super(filterIndex, description);
+		// По умолчанию выбираем все источники
+		setFilterStringLiterals(new String[]{
+			TOOL_PROXY, TOOL_REPEATER, TOOL_INTRUDER, TOOL_SCANNER, 
+			TOOL_SPIDER, TOOL_SEQUENCER, TOOL_EXTENDER, TOOL_TARGET,
+			TOOL_COMPARER, TOOL_COLLABORATOR, TOOL_LOGGER
+		});
+	}
+	
+	@Override
+	public String getInfoText() {
+		if (onOffButton != null) {
+			if (hasStringLiterals() && stringLiterals != null && stringLiterals.length > 0) {
+				String selectedTools = GenericHelper.getArrayAsString(stringLiterals);
+				return "<html>" + getDescription() + "<br><strong><em>Selected: " + selectedTools + "</em></strong></html>";
+			} else {
+				return getDescription() + "<br><strong><em>No sources selected</em></strong>";
+			}
+		}
+		return "";
+	}
+
+	@Override
+	public boolean filterRequest(IBurpExtenderCallbacks callbacks, int toolFlag, IRequestInfo requestInfo, IResponseInfo responseInfo) {
+		if(onOffButton.isSelected()) {
+			// Если ни один источник не выбран, фильтруем все запросы
+			if(stringLiterals == null || stringLiterals.length == 0) {
+				incrementFiltered();
+				return true;
+			}
+			// Проверяем, разрешен ли этот источник
+			String toolName = getToolName(toolFlag);
+			if(toolName != null && isToolAllowed(toolName)) {
+				return false; // Не фильтруем, если источник разрешен
+			}
+			// Фильтруем, если источник не разрешен или неизвестен
+			incrementFiltered();
+			return true;
+		}
+		return false; // Если фильтр выключен, не фильтруем
+	}
+	
+	private String getToolName(int toolFlag) {
+		if(toolFlag == IBurpExtenderCallbacks.TOOL_PROXY) {
+			return TOOL_PROXY;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_REPEATER) {
+			return TOOL_REPEATER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_INTRUDER) {
+			return TOOL_INTRUDER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_SCANNER) {
+			return TOOL_SCANNER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_SPIDER) {
+			return TOOL_SPIDER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_SEQUENCER) {
+			return TOOL_SEQUENCER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_EXTENDER) {
+			return TOOL_EXTENDER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_TARGET) {
+			return TOOL_TARGET;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_COMPARER) {
+			return TOOL_COMPARER;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_COLLABORATOR) {
+			return TOOL_COLLABORATOR;
+		} else if(toolFlag == IBurpExtenderCallbacks.TOOL_LOGGER) {
+			return TOOL_LOGGER;
+		}
+		return null;
+	}
+	
+	private boolean isToolAllowed(String toolName) {
+		if(stringLiterals == null) {
+			return true; // Если список пуст, разрешаем все
+		}
+		for(String allowedTool : stringLiterals) {
+			if(allowedTool != null && allowedTool.equals(toolName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public List<String> getAllAvailableTools() {
+		return Arrays.asList(
+			TOOL_PROXY, TOOL_REPEATER, TOOL_INTRUDER, TOOL_SCANNER,
+			TOOL_SPIDER, TOOL_SEQUENCER, TOOL_EXTENDER, TOOL_TARGET,
+			TOOL_COMPARER, TOOL_COLLABORATOR, TOOL_LOGGER
+		);
+	}
+
+	@Override
+	public boolean hasStringLiterals() {
+		return true;
+	}
+}
+
---
+++ src/com/protect7/authanalyzer/gui/dialog/ToolSourceDialog.java
@@ -0,0 +1,107 @@
+package com.protect7.authanalyzer.gui.dialog;
+
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+import com.protect7.authanalyzer.filter.ToolSourceFilter;
+
+public class ToolSourceDialog extends JDialog {
+	
+	private static final long serialVersionUID = 1L;
+	private final ToolSourceFilter filter;
+	private final List<JCheckBox> checkBoxes = new ArrayList<>();
+	
+	public ToolSourceDialog(Component parent, ToolSourceFilter filter) {
+		this.filter = filter;
+		setTitle("Select Request Sources");
+		
+		JPanel dialogPanel = (JPanel) getContentPane();
+		dialogPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		dialogPanel.setLayout(new GridLayout(0, 1, 5, 5));
+		
+		JLabel infoLabel = new JLabel("<html>Select which request sources should be analyzed:<br></html>");
+		dialogPanel.add(infoLabel);
+		
+		// Получаем список всех доступных инструментов
+		List<String> allTools = filter.getAllAvailableTools();
+		String[] selectedTools = filter.getFilterStringLiterals();
+		List<String> selectedToolsList = selectedTools != null ? Arrays.asList(selectedTools) : new ArrayList<>();
+		
+		// Создаем чекбоксы для каждого инструмента
+		for(String tool : allTools) {
+			JCheckBox checkBox = new JCheckBox(tool);
+			checkBox.setSelected(selectedToolsList.contains(tool));
+			checkBoxes.add(checkBox);
+			dialogPanel.add(checkBox);
+		}
+		
+		// Кнопки Select All и Deselect All
+		JPanel buttonPanel = new JPanel(new GridLayout(1, 3, 5, 5));
+		JButton selectAllButton = new JButton("Select All");
+		selectAllButton.addActionListener(e -> {
+			for(JCheckBox cb : checkBoxes) {
+				cb.setSelected(true);
+			}
+		});
+		
+		JButton deselectAllButton = new JButton("Deselect All");
+		deselectAllButton.addActionListener(e -> {
+			for(JCheckBox cb : checkBoxes) {
+				cb.setSelected(false);
+			}
+		});
+		
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(e -> {
+			saveSelection();
+		});
+		
+		buttonPanel.add(selectAllButton);
+		buttonPanel.add(deselectAllButton);
+		buttonPanel.add(okButton);
+		dialogPanel.add(buttonPanel);
+		
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setModal(true);
+		pack();
+		setLocationRelativeTo(parent);
+		setVisible(true);
+	}
+	
+	private void saveSelection() {
+		List<String> selectedTools = new ArrayList<>();
+		for(JCheckBox checkBox : checkBoxes) {
+			if(checkBox.isSelected()) {
+				selectedTools.add(checkBox.getText());
+			}
+		}
+		if(selectedTools.isEmpty()) {
+			int result = javax.swing.JOptionPane.showConfirmDialog(
+				this,
+				"No request sources selected. All requests will be filtered.\n\nDo you want to continue?",
+				"Warning",
+				javax.swing.JOptionPane.YES_NO_OPTION,
+				javax.swing.JOptionPane.WARNING_MESSAGE
+			);
+			if(result != javax.swing.JOptionPane.YES_OPTION) {
+				return; // Отменяем закрытие диалога
+			}
+		}
+		filter.setFilterStringLiterals(selectedTools.toArray(new String[0]));
+		// Обновляем подсказку после сохранения
+		if(filter.onOffButton != null) {
+			filter.onOffButton.setHint(filter.getInfoText());
+		}
+		dispose();
+	}
+}
+
